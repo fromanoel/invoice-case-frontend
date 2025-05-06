0bfa0900296a1bb4b7e4943b826d0561171080ca
fromanoel <fernanda.romanoel@estudante.iftm.edu.br>
2025-05-05 21:54:09 -0300

    style: added chat interface

--- src/components/dashboard/InvoiceHistory.tsx
+++ src/components/dashboard/InvoiceHistory.tsx
@@ -1,11 +1,25 @@
+"use client"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import UserProfile from "./UserProfile";
 import styles from "./invoiceHistory.module.css";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 export default function InvoiceHistory() {
+
+  const handleFileChange = () => {
+
+  }
   return (
     <section className={styles.invoiceHistorySection}>
       <UserProfile />
       <div>
+        <div className={styles.invoiceHistoryAndButton}>
         <h3>Invoice History</h3>
+        <input id="fileInput" type="file" accept=".png,.svg" onChange={handleFileChange} className={styles.uploadButton}/>
+        <label htmlFor="fileInput" className={styles.customFileLabel}>
+        <FontAwesomeIcon icon={faPlus}/>
+        </label>
+        </div>
+        
         <ul>
           <li>
             <a href="#">Invoice 1</a>
--- src/components/dashboard/UploadInvoice.tsx
+++ src/components/dashboard/UploadInvoice.tsx
@@ -4,11 +4,15 @@ import styles from "./UploadInvoice.module.css";
 
 export default function UploadInvoice() {
   const [imagePreview, setImagePreview] = useState<string | null>(null);
+  const [isFileUploaded, setIsFileUploaded] = useState<boolean>(false);  // Estado para controle do upload
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
     if (file) {
       setImagePreview(URL.createObjectURL(file));
+      setIsFileUploaded(true);  // Marca o arquivo como carregado
+    } else {
+      setIsFileUploaded(false);  // Se não houver arquivo, desmarca
     }
   };
 
@@ -18,20 +22,36 @@ export default function UploadInvoice() {
 
       <div className={styles.uploadInvoiceResult}>
         {imagePreview ? (
-          <img src={imagePreview} alt="Invoice preview" className={styles.previewImage} />
+          <div className={styles.divSecundaria}>
+            <div className={styles.imageDiv}>
+            <img
+              src={imagePreview}
+              alt="Invoice preview"
+              className={styles.previewImage}
+            />
+            </div>
+            <div className={`${styles.chatContainer} ${styles.iaMessage}`}>
+              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
+            </div>
+            <div className={`${styles.chatContainer} ${styles.userMessage}`}>
+              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
+            </div>
+          </div>
         ) : (
-          <div>
+          <div className={styles.uploadInvoiceBox}>
             <p>Add an invoice to start</p>
             <input
+              id="file"
               className={styles.uploadButton}
               type="file"
               accept=".png,.svg"
               onChange={handleFileChange}
             />
+            <label htmlFor="file" className={styles.customFileLabel}>
+              Choose File
+            </label>
           </div>
-          
         )}
-       
       </div>
 
       <div className={styles.uploadInvoiceInteraction}>
@@ -39,9 +59,15 @@ export default function UploadInvoice() {
           <textarea
             className={styles.uploadInvoiceTextarea}
             placeholder="Upload an invoice to start."
+            disabled={!isFileUploaded}  // Desabilita o textarea até que o arquivo seja carregado
           ></textarea>
         </label>
-        <button className={styles.sendButton}>Send</button>
+        <button
+          className={styles.sendButton}
+          disabled={!isFileUploaded}  // Desabilita o botão até que o arquivo seja carregado
+        >
+          Send
+        </button>
       </div>
     </section>
   );
